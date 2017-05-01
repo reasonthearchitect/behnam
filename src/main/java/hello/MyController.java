6340f9f0975f3e62fe7425e182971e847ef82f90
reasonthearchitect <rhys.personal@gmail.com>
2017-05-01 17:19:20 -0400

    Added the first build for Behnam

---
+++ src/main/java/hello/MyController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @GetMapping("/api/hello/{name}")
+    //@SendTo("/topic/greetings")
+    public Greeting greetingGet(@PathVariable String name) throws Exception {
+        this.template.convertAndSend("/topic/greetings", new Greeting(name));
+        return new Greeting(name+ ": should be sent");
+    }
+}
